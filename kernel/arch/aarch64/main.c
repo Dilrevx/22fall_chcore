1d1ad0698d8998c1fabf7dd23a4e52ca990e0eff
Richard Chien <stdrc@outlook.com>
2022-11-17 17:49:08 +0000

    lab3

--- kernel/arch/aarch64/main.c
+++ kernel/arch/aarch64/main.c
@@ -1,10 +1,28 @@
+/*
+ * Copyright (c) 2022 Institute of Parallel And Distributed Systems (IPADS)
+ * ChCore-Lab is licensed under the Mulan PSL v1.
+ * You can use this software according to the terms and conditions of the Mulan
+ * PSL v1. You may obtain a copy of Mulan PSL v1 at:
+ *     http://license.coscl.org.cn/MulanPSL
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE. See the
+ * Mulan PSL v1 for more details.
+ */
+
+#include <sched/sched.h>
 #include <common/kprint.h>
 #include <common/vars.h>
 #include <common/macro.h>
 #include <common/types.h>
+#include <common/lock.h>
+#include <arch/machine/smp.h>
+#include <arch/mm/page_table.h>
 #include <mm/mm.h>
 #include <io/uart.h>
 #include <machine.h>
+#include <irq/irq.h>
+#include <object/thread.h>
 
 ALIGN(STACK_ALIGNMENT)
 char kernel_stack[PLAT_CPU_NUM][KERNEL_STACK_SIZE];
@@ -27,6 +45,8 @@ static void lab2_test_kernel_vaddr(void)
  */
 void main(paddr_t boot_flag)
 {
+        u32 ret = 0;
+
         /* Init uart: no need to init the uart again */
         uart_init();
         kinfo("[ChCore] uart init finished\n");
@@ -46,8 +66,15 @@ void main(paddr_t boot_flag)
         lab2_test_page_table();
 #endif /* CHCORE_KERNEL_TEST */
 
-        while (1) {
-        }
+        /* Init exception vector */
+        arch_interrupt_init();
+        kinfo("[ChCore] interrupt init finished\n");
+
+        create_root_thread();
+        kinfo("[ChCore] create initial thread done on %d\n", smp_get_cpu_id());
+
+        /* Context switch to the picked thread */
+        eret_to_thread(switch_context());
 
         /* Should provide panic and use here */
         BUG("[FATAL] Should never be here!\n");
