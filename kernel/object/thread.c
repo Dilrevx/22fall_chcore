e5c1ef8b5d08050af6305dbe5b86f07fb4bbde75
520030910241 <daat00@sjtu.edu.cn>
2022-12-08 11:50:09 -0800

    finish: before sys_call

--- kernel/object/thread.c
+++ kernel/object/thread.c
@@ -108,7 +108,24 @@ static u64 load_binary(struct cap_group *cap_group, struct vmspace *vmspace,
                         seg_sz = elf->p_headers[i].p_memsz;
                         p_vaddr = elf->p_headers[i].p_vaddr;
                         /* LAB 3 TODO BEGIN */
+                        seg_map_sz = ROUND_UP((p_vaddr + seg_sz), PAGE_SIZE)
+                                     - ROUND_DOWN(p_vaddr, PAGE_SIZE);
 
+                        pmo_cap[i] = create_pmo(
+                                seg_map_sz, PMO_DATA, cap_group, &pmo);
+                        memset(phys_to_virt(pmo->start), 0, pmo->size);
+                        memcpy(phys_to_virt(pmo->start)
+                                       + (OFFSET_MASK & p_vaddr),
+                               bin + elf->p_headers[i].p_offset,
+                               elf->p_headers[i].p_filesz);
+                        // OFFSET_MASK : consider offset in one page
+
+                        flags = PFLAGS2VMRFLAGS(elf->p_headers[i].p_flags);
+                        ret = vmspace_map_range(vmspace,
+                                                ROUND_DOWN(p_vaddr, PAGE_SIZE),
+                                                seg_map_sz,
+                                                flags,
+                                                pmo);
                         /* LAB 3 TODO END */
                         BUG_ON(ret != 0);
                 }
@@ -399,7 +416,7 @@ void sys_thread_exit(void)
         printk("\nBack to kernel.\n");
 #endif
         /* LAB 3 TODO BEGIN */
-
+        
         /* LAB 3 TODO END */
         printk("Lab 3 hang.\n");
         while (1) {
