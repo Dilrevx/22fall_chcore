414994e4946392167c64cf1c3ca4aa77a80b1ac7
520030910241 <daat00@sjtu.edu.cn>
2022-12-09 05:43:46 -0800

    finish lab3

--- kernel/object/thread.c
+++ kernel/object/thread.c
@@ -416,7 +416,8 @@ void sys_thread_exit(void)
         printk("\nBack to kernel.\n");
 #endif
         /* LAB 3 TODO BEGIN */
-        
+        current_thread->thread_ctx->state = TS_EXIT;
+        current_thread = NULL;
         /* LAB 3 TODO END */
         printk("Lab 3 hang.\n");
         while (1) {
